ba75212d0aa8a81c733cbb7a1198b8e648045c50
gyeung <gyeung@cs.stonybrook.edu>
2018-04-23 19:00:57 -0400

    Update RandomClassifier.java

--- hw1/data-vilij/src/classification/RandomClassifier.java
+++ hw1/data-vilij/src/classification/RandomClassifier.java
@@ -14,9 +14,13 @@ import algorithm.DataSet;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
+import ui.AppUI;
+import vilij.templates.ApplicationTemplate;
 
 public class RandomClassifier extends Classifier {
 
@@ -31,6 +35,7 @@ public class RandomClassifier extends Classifier {
 
     // currently, this value does not change after instantiation
     private final AtomicBoolean tocontinue;
+    private Class AppUI;
 
     @Override
     public int getMaxIterations() {
@@ -48,37 +53,36 @@ public class RandomClassifier extends Classifier {
     }
 
     public RandomClassifier(DataSet dataset,
-                            int maxIterations,
-                            int updateInterval,
-                            boolean tocontinue) {
+            int maxIterations,
+            int updateInterval,
+            boolean tocontinue) {
         this.dataset = dataset;
         this.maxIterations = maxIterations;
         this.updateInterval = updateInterval;
         this.tocontinue = new AtomicBoolean(tocontinue);
     }
 
+    public ArrayList<Integer> getDataOutput() {
+        return (ArrayList<Integer>) output;
+    }
+
     @Override
     public void run() {
-        for (int i = 1; i <= maxIterations && tocontinue(); i++) {
-            int xCoefficient = new Double(RAND.nextDouble() * 100).intValue();
-            int yCoefficient = new Double(RAND.nextDouble() * 100).intValue();
-            int constant     = new Double(RAND.nextDouble() * 100).intValue();
+        int xCoefficient = new Double(RAND.nextDouble() * 100).intValue();
+        int yCoefficient = new Double(RAND.nextDouble() * 100).intValue();
+        int constantVal = new Double(RAND.nextDouble() * 100).intValue();
+        // this is the real output of the classifier
 
-            // this is the real output of the classifier
-            output = Arrays.asList(xCoefficient, yCoefficient, constant);
-
-            // everything below is just for internal viewing of how the output is changing
-            // in the final project, such changes will be dynamically visible in the UI
-            if (i % updateInterval == 0) {
-                System.out.printf("Iteration number %d: ", i); //
-                flush();
-            }
-            if (i > maxIterations * .6 && RAND.nextDouble() < 0.05) {
-                System.out.printf("Iteration number %d: ", i);
-                flush();
-                break;
-            }
-        }
+        output = (List<Integer>) new ArrayList<Integer>();
+        Integer xCoeff = new Integer(xCoefficient);
+        Integer yCoeff = new Integer(yCoefficient);
+        Integer constantValue = new Integer(constantVal);
+        output.add(xCoeff);
+        output.add(yCoeff);
+        output.add(constantValue);
+        //output = Arrays.asList(xCoefficient, yCoefficient, constant);
+        // everything below is just for internal viewing of how the output is changing
+        // in the final project, such changes will be dynamically visible in the UI
     }
 
     // for internal viewing only
@@ -86,9 +90,11 @@ public class RandomClassifier extends Classifier {
         System.out.printf("%d\t%d\t%d%n", output.get(0), output.get(1), output.get(2));
     }
 
-    /** A placeholder main method to just make sure this code runs smoothly */
+    /**
+     * A placeholder main method to just make sure this code runs smoothly
+     */
     public static void main(String... args) throws IOException {
-        DataSet          dataset    = DataSet.fromTSDFile(Paths.get("/path/to/some-data.tsd"));
+        DataSet dataset = DataSet.fromTSDFile(Paths.get("/path/to/some-data.tsd"));
         RandomClassifier classifier = new RandomClassifier(dataset, 100, 5, true);
         classifier.run(); // no multithreading yet
     }
